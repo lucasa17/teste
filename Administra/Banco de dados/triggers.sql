47871089fb83d79503fb40f21a4cedb2be41de10
lucasa17 <134516774+lucasa17@users.noreply.github.com>
2025-07-11 08:52:42 -0300

    Update triggers.sql

--- Administra/Banco de dados/triggers.sql
+++ Administra/Banco de dados/triggers.sql
@@ -3,7 +3,7 @@ DELIMITER //
 CREATE TRIGGER tr_seta_data_vencimento 
 BEFORE INSERT ON Divida 
 FOR EACH ROW
-BEGIN 
+BEGIN  	
 	IF (NEW.data_vencimento IS NULL) THEN
 		SET NEW.data_vencimento = DATE_ADD(NEW.data_primeira_parcela, INTERVAL (NEW.parcelas - 1) MONTH );
 	END IF;
